5d2023182604647c254bce7a4b02a0c9aea7dacd
Mateus3F <mateus@3fventure.com.br>
2026-02-03 15:27:32 -0300

    fix: troquei porta do server.js

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 
 app.use(routes);
 
-const PORT = 3000;
+const PORT = 8021;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`➜ Server is running on port ${PORT}`);
     console.log(`➜ Local:            http://localhost:${PORT}`);
